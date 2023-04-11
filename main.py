89b9f65c2f1bdc17021ca2545b80f39159c870eb
rafalordi <rafaellordi@outlook.com.br>
2023-04-11 17:21:26 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-import matplotlib.pyplot as plt
 from PIL import Image
 
-file = 'Banco de Dados/Combustiveis.csv'
+file = 'Banco de Dados/Teste.csv'
 
 @st.cache_data
 def load_data():
     df = pd.read_csv(file, encoding='latin-1', sep = ';')
-
 
     return df
 df = load_data()
@@ -20,7 +17,7 @@ image = Image.open('Fotos/univesp.png')
 with st.columns(3)[1]:
     st.image(image, use_column_width='auto')
 
-st.title("Preço dos combustiveis 2023")
+st.title("Honest Fuel – Análises de Preços de Combustíveis")
 st.markdown(
     """
     :bar_chart: Dashboard
@@ -36,19 +33,19 @@ if st.sidebar.checkbox("Mostrar tabela"):
 customer_type = st.sidebar.multiselect(
     "Selecione o combustivel:",
     options=df["Produto"].unique(),
-    default=df["Produto"].unique(),
+    #default=df["Produto"].unique(),
 )
 
 gender = st.sidebar.multiselect(
     "Selecione a unidade de medida:",
     options=df["Unidade_de_Medida"].unique(),
-    default=df["Unidade_de_Medida"].unique()
+    #default=df["Unidade_de_Medida"].unique()
 )
 
 city = st.sidebar.multiselect(
   "Selecio o municipio:",
    options=df["Municipio"].unique(),
-   default=df["Municipio"].unique()
+   #default=df["Municipio"].unique()
 )
 
 df_selection = df.query(
@@ -56,10 +53,13 @@ df_selection = df.query(
 )
 
 chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+    np.random.randn(20, 4),
+    columns=['Gasolina', 'Etanol', 'Diesel', 'GNV'])
+
+st.bar_chart(df, y='Produto', x='Valor_de_Venda')
 
 st.line_chart(chart_data)
 
 st.bar_chart(chart_data)
+
 
